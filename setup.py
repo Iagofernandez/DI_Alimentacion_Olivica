a30edecb02bf9c3ffc0caf337ea8b6e70f764c25
iago <stevinterlerez@gmail.com>
2020-03-26 17:29:55 +0100

    Generado un setup.py Actualizacion del proyecto

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='Hello,',
+setuptools.setup\
+      (name='Iago Fernandez Blanco',
       version='1.0',
       description='TiendaOlivica',
       author='Iago Fernández Blanco',
+       packages = setuptools.find_packages(),
+       classifiers=[
+             "Programming Language :: Python :: 3",
+             "License :: OSI Approved :: MOT License"
+       ],
+       python_requires ='>=3.6',
       )
